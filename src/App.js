2631af9df7a92074555d4aa6d3d24a4429706fd6
Jeremy Greer <jex.grizzle@gmail.com>
2019-01-08 16:26:43 -0500

    Pass list of notes to staff and they are turned on

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ const Space = (props) => (
   </div>
 )
 
-
 const Staff = (props) => {
   return (
     <div className={`Staff ${ props.treble ? "TrebleCleff" : "BassCleff" }`}>
@@ -32,7 +31,11 @@ const Staff = (props) => {
         props.range.map((name, i) => {
           const Child = i % 2 === 0 ? Space : Line
           return (
-            <Child key={name} name={name} />
+            <Child
+              key={name}
+              name={name}
+              on={props.on.includes(name)}
+            />
           )
         })
       }
@@ -43,24 +46,27 @@ const Staff = (props) => {
 const Treble = (props) => {
   const range = helper.getRangeWithOctaves('D4G5').reverse()
   return (
-    <Staff treble range={range} />
+    <Staff treble range={range} on={props.on} />
   )
 }
 
 const Bass = (props) => {
   const range = helper.getRangeWithOctaves('F2B4').reverse()
   return (
-    <Staff bass range={range} />
+    <Staff bass range={range} on={props.on} />
   )
 }
 
-
 class App extends Component {
+  state = {
+    on: ['B3', 'F4', 'G5'],
+  }
+
   render() {
     return (
       <div className="App">
-        <Treble />
-        <Bass />
+        <Treble on={this.state.on} />
+        <Bass on={this.state.on} />
       </div>
     );
   }
